b75229cdc8b5e25481df1ebf498109ef4ff9cde0
RitaHC <ritahameerchauhan@ritas-air.mynetworksettings.com>
2024-02-29 13:31:57 -0500

    Movies CRUD complete and tested with postman using seed data

--- main.go
+++ main.go
@@ -58,8 +58,8 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	for _, item := range movies {
-		fmt.Println(item)
 		if item.ID == params["id"] {
+			fmt.Println("One Movie Found", item)
 			json.NewEncoder(w).Encode(item)
 			return
 		}
@@ -84,7 +84,7 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 }
 
 // Update Movie
-func updateMovie() {
+func updateMovie(w http.ResponseWriter, r *http.Request) {
 
 	// set json content type
 	w.Header().Set("Content-Type", "application/json")
@@ -94,7 +94,20 @@ func updateMovie() {
 
 	// delete the movie with the id that we have send
 	// add a new movie - which is the movie we've send via postman
+	for index, item := range movies {
+		//Step 1 : find and delete the old movie
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			// Step 2:  Now create new movie
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
 
+	}
 }
 
 func main() {
@@ -106,7 +119,7 @@ func main() {
 	movies = append(movies, Movie{ID: "2", Isbn: "264", Title: "Movie Two", Director: &Director{Firstname: "Rajat", Lastname: "Kumar"}})
 
 	r.HandleFunc("/movies", getMovies).Methods("GET")
-	r.HandleFunc("/moies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
 	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
